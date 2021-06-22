134a1b1fef0af2b410eed1213a8458e5650f83b9
Ярослав <bankakonservov@mail.ru>
2021-06-22 19:02:38 +0500

    fix progect tests

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -13,7 +13,7 @@ class StaticPagesURLTests(TestCase):
         self.assertEqual(
             response.status_code, HTTPStatus.OK)
 
-        response = self.guest_client.get('/second/')
+        response = self.guest_client.get('/second_page/')
         self.assertEqual(
             response.status_code, HTTPStatus.OK)
 
@@ -22,5 +22,5 @@ class StaticPagesURLTests(TestCase):
         response = self.guest_client.get('/')
         self.assertContains(response, 'У меня получилось!')
 
-        response = self.guest_client.get('/second/')
+        response = self.guest_client.get('/second_page/')
         self.assertContains(response, 'А это вторая страница!')
